64577d6a7edf2a64893ce8ea47f14f856ca42b61
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-06-14 17:30:38 +0100

    weather img

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,8 +1,20 @@
 import requests
 def getWeather(city):
-    api = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ba20c35910ea8cfa41785d81baafa2c9"
+    api = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ba20c35910ea8cfa41785d81baafa2c9"
     data = requests.get(api).json()
+    print(data["weather"][0]['description'])
     print(data)
-    print()
-    print(requests.get(api))
+    
 getWeather("Glasgow")
+
+from tkinter import *
+from tkinter import messagebox
+top = Tk()
+
+C = Canvas(top, bg="blue", height=250, width=300)
+filename = PhotoImage(file = "/Images/Cloudy.jpg")
+background_label = Label(top, image=filename)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+C.pack()
+top.mainloop
