a8e34dc1c47ab003a4b642416a9154658e4fffd0
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-06-14 17:51:31 +0100

    new images

--- WeatherApp.py
+++ WeatherApp.py
@@ -4,17 +4,31 @@ def getWeather(city):
     data = requests.get(api).json()
     print(data["weather"][0]['description'])
     print(data)
+    return data
     
-getWeather("Glasgow")
+data = getWeather("Glasgow")
 
 from tkinter import *
-from tkinter import messagebox
-top = Tk()
+from PIL import ImageTk, Image
+root = Tk()
 
-C = Canvas(top, bg="blue", height=250, width=300)
-filename = PhotoImage(file = "Images/Cloudy.jpg")
-background_label = Label(top, image=filename)
+C = Canvas(root, bg="blue", height=250, width=300)
+
+
+if data["weather"][0]['main'] == "Clear":
+    img = ImageTk.PhotoImage(Image.open("Images\clear.jpg"))
+elif data["weather"][0]['main'] == "Clouds":
+    img = ImageTk.PhotoImage(Image.open("Images\cloud.jpg"))
+else:
+    img = ImageTk.PhotoImage(Image.open("Images\storm.jpg"))
+
+background_label = Label(root, image=img)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+weatherDesc = Label(text=data["weather"][0]['description'])
+weatherDesc.pack()
+
+
+
 C.pack()
-top.mainloop
+mainloop()
