8c0421952bf1199ff1db0d51242d0d720cc20c7a
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-06-14 17:32:27 +0100

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -12,7 +12,7 @@ from tkinter import messagebox
 top = Tk()
 
 C = Canvas(top, bg="blue", height=250, width=300)
-filename = PhotoImage(file = "/Images/Cloudy.jpg")
+filename = PhotoImage(file = "Images/Cloudy.jpg")
 background_label = Label(top, image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
